c49c2b9b5bbbd5366da9581470651545a7e878d6
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-06-03 13:15:20 +0900

    Update 5th.c

--- 5th.c
+++ 5th.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+void main()
+{
+  char str[20];
+  int i;
+  printf("20글자 이하의 단어(영어)를 입력해주세요:");
+  scanf("%s",&str);
+  for(i=0;str[i]!='\0';i++)
+  {
+    printf("%c\n",str[i]);
+  }
+}
