8ed59095a000d5d293b9f27a83d3b3a9ac29c580
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-06-03 12:47:53 +0900

    Update 4th.c

--- 4th.c
+++ 4th.c
@@ -10,12 +10,16 @@ printf("%d.%02d.%02d",a,b,c);
 
 
 char st[15];
-
+int i;
+  
 printf("주민등록번호를 '-'를 사용해 입력해주세요:");
 scanf("%s",&st);
 
-if (st!='-')
+if (st[6]=='-')
 {
+  for(i=6;i<15;i++)
+    st[i]=st[i+1];
+}
 
 printf("%s",st);
 
