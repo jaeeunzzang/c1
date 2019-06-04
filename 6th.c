e78d8e790b340196c3641f417383cfc31f76e257
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-06-04 09:33:27 +0900

    Update 6th.c

--- 6th.c
+++ 6th.c
@@ -13,3 +13,22 @@ scanf("%c\n",&a);
 printf("%c\n",a);
 return 0;
 }
+
+/* 모범소스
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    while(1)
+    {
+        scanf("%c ", &a);
+        if(a != 'q') printf("%c\n",a);
+        else {
+            printf("q");
+            return 0;
+        }
+    }
+}
+
+*/
