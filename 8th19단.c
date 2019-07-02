0df270c20b7a39a463ebde0a751c1747517b11f2
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-02 13:25:30 +0900

    Update 8th19단.c

--- 8th19단.c
+++ 8th19단.c
@@ -4,23 +4,23 @@ int main()
 {
   int a[20][19][3] ={0};
   int i,j;
-  int sum;
+  int sum=0;
   
-  for(i=0;i<20;i++)
+  for(i=1;i<20;i++)
   {
     for(j=0;j<19;j++)
     {
-       a[i][j][0] = i+1;
+       a[i][j][0] = i;
       a[i][j][1] = j+1;
-      a[i][j][2]= (i+1)*(j+1);
+      a[i][j][2]= (i)*(j+1);
     }
   }
   
   printf("구구단을 출력합니다.\n");
   
-  for(i=0;i<20;i++)
+  for(i=1;i<20;i++)
   {
-    printf("%d 단\n",i+1);
+    printf("%d 단\n",i);
     
     for(j=0;j<19;j++)
     {
@@ -39,7 +39,7 @@ int main()
   {
     for(j=0;j<19;j++)
     {
-      sum= a[i][j][0]+a[i][j][1]+a[i][j][2];
+      sum= sum+a[i][j][0]+a[i][j][1]+a[i][j][2];
     }
   }
   printf("합계 : %d\n",sum);
